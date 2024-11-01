94d0fd0413ee5c20a749f47e426132c20f9fb988
Joshua Ebert <ebert.joshua@protonmail.com>
2024-11-01 12:34:55 +0100

    Start endpoint will now take in an end time

--- src/main/kotlin/jetzt/arbeitszeitbetrug/app/controller/v1/StartController.kt
+++ src/main/kotlin/jetzt/arbeitszeitbetrug/app/controller/v1/StartController.kt
@@ -1,16 +1,25 @@
 package jetzt.arbeitszeitbetrug.app.controller.v1
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.util.logging.*
+import io.ktor.utils.io.*
 import jetzt.arbeitszeitbetrug.app.currentlyActiveUsers
+import kotlinx.serialization.Serializable
+import kotlin.time.TimeMark
 
 object StartController {
 
     private val LOGGER = KtorSimpleLogger("StartController")
 
-    fun start(call: ApplicationCall, body: Unit) {
+    @Serializable
+    data class StartRequest(val endTime: String)
+
+    suspend fun start(call: ApplicationCall, body: StartRequest) {
         LOGGER.debug("StartController.start called")
         currentlyActiveUsers++
+        call.respond(HttpStatusCode.OK)
     }
 
 }
--- src/main/kotlin/jetzt/arbeitszeitbetrug/app/routes/v1/ControlRoute.kt
+++ src/main/kotlin/jetzt/arbeitszeitbetrug/app/routes/v1/ControlRoute.kt
@@ -6,11 +6,23 @@ import jetzt.arbeitszeitbetrug.app.controller.v1.StartController
 import jetzt.arbeitszeitbetrug.app.routes.API_PATH
 import jetzt.arbeitszeitbetrug.app.routes.API_VERSION
 import jetzt.arbeitszeitbetrug.app.util.Validator
+import jetzt.arbeitszeitbetrug.app.util.Validators.isTime
+import jetzt.arbeitszeitbetrug.app.util.Validators.notEmpty
+import jetzt.arbeitszeitbetrug.app.util.validatorChain
 
 const val START_ROUTE = "$API_PATH$API_VERSION/start"
 
+val startValidator = validatorChain<StartController.StartRequest> {
+    body {
+        field({ it.endTime }) {
+            +notEmpty()
+            +isTime()
+        }
+    }
+}
+
 fun Route.start() {
     post(START_ROUTE) {
-        pass(call, Validator.None, StartController::start)
+        pass(call, startValidator, StartController::start)
     }
 }
